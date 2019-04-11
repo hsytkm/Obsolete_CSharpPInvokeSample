88bf91be1dc79e312f14d3d9434155a46ea76420
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-04-11 20:45:37 +0900

    add files

---
+++ CoreConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CoreConsole
+{
+    class Program
+    {
+        [DllImport("NativeWinLib.dll")]
+        public extern static int GetInt();
+
+
+        static void Main(string[] args)
+        {
+            var x = GetInt();
+            Console.WriteLine($"Hello World {x} !");
+        }
+    }
+}
---
+++ NativeWinLib/dllmain.cpp
@@ -0,0 +1,12 @@
+﻿// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
+#include "pch.h"
+
+#define DllExport __declspec( dllexport )
+
+extern "C" {
+
+	DllExport int GetInt() {
+		return 1234;
+	}
+
+}
